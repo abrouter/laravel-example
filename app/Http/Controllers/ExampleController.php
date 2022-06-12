0c5e1fae4c51e485dea85920822aa383d72a9f87
Oleksii Yatsenko <yatsenkolesh@gmail.com>
2022-06-12 20:05:56 +0100

    fix

--- app/Http/Controllers/ExampleController.php
+++ app/Http/Controllers/ExampleController.php
@@ -4,14 +4,27 @@ declare(strict_types = 1);
 namespace App\Http\Controllers;
 
 use Abrouter\Client\Client;
+use Abrouter\Client\DTO\EventDTO;
 
 class ExampleController
 {
     public function __invoke(Client $client)
     {
-        $buttonColor = $client->experiments()->run(uniqid(), 'D1D06000-0000-0000-00005030');
-        return view('button', [
-            'color' => $buttonColor->getBranchId(),
-        ]);
+        try {
+            $userId = uniqid();
+            $buttonColor = $client->experiments()->run($userId, 'button_color');
+            $client->statistics()->sendEvent(new EventDTO(
+                null,
+                $userId,
+                'visited_test_page'
+            ));
+
+            return view('button', [
+                'color' => $buttonColor->getBranchId(),
+            ]);
+        } catch (\Exception $e) {
+            die(var_dump($e->getMessage()));
+        }
+
     }
 }
